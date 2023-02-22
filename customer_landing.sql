4742f040938170288d5b1fb14e4952cd6c29a089
sevkw <kwang0422@gmail.com>
2023-02-22 00:23:54 -0500

    add customer_landing

---
+++ customer_landing.sql
@@ -0,0 +1,22 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `kerry-stedi-lakehouse`.`customer_landing` (
+  `serialnumber` string,
+  `sharewithpublicasofdate` bigint,
+  `birthday` date,
+  `registrationdate` bigint,
+  `sharewithresearchasofdate` bigint,
+  `customername` string,
+  `email` string,
+  `lastupdatedate` bigint,
+  `phone` string,
+  `sharewithfriendsasofdate` bigint
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://kerry-pj3-stedi-lakehouse/customer/landing/'
+TBLPROPERTIES ('classification' = 'json');
